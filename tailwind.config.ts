bfdb2c1d1ef880cb3aec940338a47db2a215a6c5
Lucas Pinheiro <lucas.gpinheiro2002@gmail.com>
2024-07-07 00:14:48 -0300

    update: theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,40 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      borderColor: {
+        DEFAULT: "#323232",
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+          primary: "#808080",
+          "primary-content": "#ccc",
+          "primary-focus": "#fff",
+          secondary: "#fff",
+          "secondary-content": "#007acc",
+          "secondary-focus": "#3a3d41",
+          accent: "#007acc",
+          "accent-content": "#fff",
+          "accent-focus": "#D4D4D8",
+          neutral: "#fff",
+          "neutral-content": "#000",
+          "neutral-focus": "#3a3d41",
+          "base-100": "#1e1e1e",
+          "base-200": "#212121",
+          "base-300": "#181818",
+          "base-content": "#ccc",
+          info: "#0284c7",
+          success: "#53c22b",
+          warning: "#e6c029",
+          error: "#ff5a52",
+        },
+      },
+    ],
+  },
 };
 export default config;
